b5ba5a1b910142978483958312b066026a58e81b
tehhiper1 <admin@whorrhouse.com>
2012-04-23 13:43:34 -0500

    Adding variables for AccountSettings object.

--- PesterchumProtocol/src/com/whorrhouse/homestuck/pesterchum/proto/AccountSettings.java
+++ PesterchumProtocol/src/com/whorrhouse/homestuck/pesterchum/proto/AccountSettings.java
@@ -2,5 +2,12 @@ package com.whorrhouse.homestuck.pesterchum.proto;
 
 public class AccountSettings
 {
-
+	private boolean pushLogsToServer;
+	private boolean pushQuirksToServer;
+	private boolean pushChumRollToServer;
+	private int selectedTheme;
+	public AccountSettings()
+	{
+		
+	}
 }
