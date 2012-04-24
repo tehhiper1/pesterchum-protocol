c476853392b951d3c2e8975de8ae0618a05f3bba
tehhiper1 <admin@whorrhouse.com>
2012-04-24 13:39:00 -0500

    added javadocs

--- PesterchumProtocol/src/com/whorrhouse/homestuck/pesterchum/proto/AccountSettings.java
+++ PesterchumProtocol/src/com/whorrhouse/homestuck/pesterchum/proto/AccountSettings.java
@@ -1,5 +1,11 @@
 package com.whorrhouse.homestuck.pesterchum.proto;
 
+/**
+ * Class AccountSettings contains information about what settings are toggled on and off for a particular account. These include whether or not the user elects to push its pesterlogs, quirks, and chumroll to the server. It also contains the last used theme that the user selected.
+ * @see Account
+ * @author tehhiper1
+ * @version 1.0a
+ */
 public class AccountSettings
 {
 	private boolean pushLogsToServer;
