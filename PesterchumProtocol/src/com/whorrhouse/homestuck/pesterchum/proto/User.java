c0912b9a84deba090e290d1789181c639ca2385e
tehhiper1 <admin@whorrhouse.com>
2012-04-24 13:39:31 -0500

    added javadocs

--- PesterchumProtocol/src/com/whorrhouse/homestuck/pesterchum/proto/User.java
+++ PesterchumProtocol/src/com/whorrhouse/homestuck/pesterchum/proto/User.java
@@ -1,6 +1,17 @@
 package com.whorrhouse.homestuck.pesterchum.proto;
 
+/**
+ * A User contains information about its specific chum handle, chat color, whether or not they are flagged as a troll andother information
+ * @see PCProtocol
+ * @see Account
+ * @author tehhiper1
+ * @version 1.0
+ */
+
 public class User
 {
-
+	public User()
+	{
+		
+	}
 }
